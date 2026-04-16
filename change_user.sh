c047a1ad97ac795b28adc61b119376017a073762
aerosmooth <sasa0309sho@gmail.com>
2026-04-16 17:55:24 +0900

    modify change_user.sh

--- change_user.sh
+++ change_user.sh
@@ -1,29 +1,16 @@
-# git filter-branch -f --env-filter '
-# OLD_EMAIL="sasa0309sho@gmail.com"
-# CORRECT_NAME="aerosmooth"
-# CORRECT_EMAIL="sasa0309sho@gmail.com"
-#
-# if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-# then
-#     export GIT_COMMITTER_NAME="$CORRECT_NAME"
-#     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-# fi
-# if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-# then
-#     export GIT_AUTHOR_NAME="$CORRECT_NAME"
-#     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-# fi
-# ' --tag-name-filter cat -- --branches --tags
-git filter-branch --env-filter '
+git filter-branch -f --env-filter '
+OLD_EMAIL="sasa0309sho@gmail.com"
+CORRECT_NAME="aerosmooth"
+CORRECT_EMAIL="sasa0309sho@gmail.com"
 
-OLD_NAME="sasa0309sho-blip"
-NEW_NAME="aerosmooth"
-
-if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]; then
-    export GIT_AUTHOR_NAME="$NEW_NAME"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-
-if [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ]; then
-    export GIT_COMMITTER_NAME="$NEW_NAME"
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
 ' --tag-name-filter cat -- --branches --tags
